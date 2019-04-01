09f716db4f64b1bc4422cc2ac0f96a05eb5e3a19
juneju <djuneju@gmail.com>
2019-04-02 03:22:13 +0600

    Thematic Break Added in Event List

--- EventBook/resources/views/Events/index.blade.php
+++ EventBook/resources/views/Events/index.blade.php
@@ -2,20 +2,19 @@
 
 @section('content')
 
-
     <h1>Events</h1>
 
     @if(count($events)>0)
-@foreach($events as $event)
-<div class= 'well'>
-<h3><a href ="/events/{{$event->id}}">{{$event->name}}</a></h3>
-<small>Deadline: {{$event->reg_deadline}}</small>
-</div>
-@endforeach
-@else 
-
-<p>No Events Found</p>
-@endif
+    @foreach($events as $event)
+        <div class= 'well' >
+        <h3><a href ="/events/{{$event->id}}">{{$event->name}}</a></h3>
+        <small>Deadline: {{$event->reg_deadline}}</small>
+        <hr>
+        </div>
+    @endforeach
+    @else 
+        <p>No Events Found</p>
+    @endif
 
 @endsection
 
