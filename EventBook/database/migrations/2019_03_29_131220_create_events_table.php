80cdaf2474ada828bec4b4adc11d8608d445108f
FedTheBug <fahadmuntasir747@gmail.com>
2019-04-03 21:24:22 +0600

    Table Modified

--- EventBook/database/migrations/2019_03_29_131220_create_events_table.php
+++ EventBook/database/migrations/2019_03_29_131220_create_events_table.php
@@ -21,8 +21,6 @@ class CreateEventsTable extends Migration
             $table->date('reg_deadline');
             $table->mediumText('description');
             $table->integer('user_id')->nullable();
-            
-            
             $table->timestamps();
         });
     }
