6166b1532114b6d84aa749ab7d032e5e996e266b
FedTheBug <fahadmuntasir747@gmail.com>
2019-04-03 19:42:36 +0600

    up() function defined

--- EventBook/database/migrations/2019_04_03_133607_add_user_id_to_events.php
+++ EventBook/database/migrations/2019_04_03_133607_add_user_id_to_events.php
@@ -14,7 +14,7 @@ class AddUserIdToEvents extends Migration
     public function up()
     {
         Schema::table('events', function (Blueprint $table) {
-            //
+            $table->integer ('user_id');
         });
     }
 
