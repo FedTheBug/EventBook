5a1c4e25903b07450c5db533b21ea4722a51b98c
juneju <djuneju@gmail.com>
2019-04-05 01:11:10 +0600

    Font Visibility Changed

--- EventBook/resources/views/pages/about.blade.php
+++ EventBook/resources/views/pages/about.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
 <body background={{ asset('img/logo1.jpg')}}>
-        <h1>{{$events}}</h1>
-        <p>Largest platform to book your events, seminars, competitions around the country.One can register for the upcoming events.</p>
+        <h1 style="color:lightblue;">{{$events}}</h1>
+        <p style="color:lightblue;">Largest platform to book your events, seminars, competitions around the country.One can register for the upcoming events.</p>
 </body>
 @endsection
