004a3c36dd90c3c490233878ca90cd15ee08ae48
juneju <djuneju@gmail.com>
2019-04-04 23:30:25 +0600

    Background Addeed

--- EventBook/resources/views/pages/index.blade.php
+++ EventBook/resources/views/pages/index.blade.php
@@ -1,13 +1,12 @@
 @extends('layouts.app')
 @section('content')
 <div class ="text-center">
-    <body>
+    <body background={{ asset('img/logo1.jpg')}}>
         <h1>Welcome to Eventbook</h1>
         <p>This is the largest platform for upcoming events.Book your events now</p>
         <p><a class ="btn btn-primary btn-lg" href="/login" role= "button">login</a> 
             <a class= "btn btn-succes btn-lg" href= "/register" role= "button">Register</a>
         </p>
     </body>
-
 </div>
 @endsection
