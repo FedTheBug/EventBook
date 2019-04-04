ff0c2a8a97f4d85f28cdcbebebb00859bf1c6878
juneju <djuneju@gmail.com>
2019-04-04 23:15:50 +0600

    <body> Added for Background in Index Class

--- EventBook/resources/views/pages/index.blade.php
+++ EventBook/resources/views/pages/index.blade.php
@@ -1,12 +1,13 @@
 @extends('layouts.app')
 @section('content')
 <div class ="jumbotron text-center">
+    <body>
+        <h1>Welcome to Eventbook</h1>
+        <p>This is the largest platform for upcoming events.Book your events now</p>
+        <p><a class ="btn btn-primary btn-lg" href="/login" role= "button">login</a> 
+            <a class= "btn btn-succes btn-lg" href= "/register" role= "button">Register</a>
+        </p>
+    </body>
 
-    <h1>Welcome to Eventbook</h1>
-    <p>This is the largest platform for upcoming events.Book your events now</p>
-    <p><a class ="btn btn-primary btn-lg" href="/login" role= "button">login</a> 
-        <a class= "btn btn-succes btn-lg" href= "/register" role= "button">Register</a>
-    </p>
-    
 </div>
 @endsection
