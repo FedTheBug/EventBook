63b05d758dc130dc1d1965af361912ecca6e7f84
juneju <djuneju@gmail.com>
2019-04-05 01:47:57 +0600

    Edit With Pre-field Value is Enabled

--- EventBook/resources/views/Events/edit.blade.php
+++ EventBook/resources/views/Events/edit.blade.php
@@ -15,23 +15,23 @@
                </div>
                <div class="form-group">
                        {{ Form::label('venue','Venue')}}
-                       {{ Form::text('venue', '',['class'=>'form-control', 'placeholder'=>'Venue'])}}
+                       {{ Form::text('venue', $event->venue ,['class'=>'form-control', 'placeholder'=>'Venue'])}}
                </div>
                <div class="form-group">
                        {{ Form::label('event_date','Event Date')}}
-                       {{ Form::date('event_date', '',['class'=>'form-control'])}}
+                       {{ Form::date('event_date', $event->event_date,['class'=>'form-control'])}}
                </div>
                <div class="form-group">
                        {{ Form::label('reg_deadline','Registration Deadline')}}
-                       {{ Form::date('reg_deadline', '',['class'=>'form-control'])}}
+                       {{ Form::date('reg_deadline', $event->reg_deadline,['class'=>'form-control'])}}
                </div>
                <div class="form-group">
                         {{ Form::label('event_type','Event Type')}}
-                        {{ Form::text('event_type', '',['class'=>'form-control', 'placeholder'=>'Event Type'])}}
+                        {{ Form::text('event_type', $event->event_type,['class'=>'form-control', 'placeholder'=>'Event Type'])}}
                 </div>
                <div class="form-group">
                        {{ Form::label('description','Description')}}
-                       {{ Form::textarea('description', '',['class'=>'form-control', 'placeholder'=>'Write a description'])}}
+                       {{ Form::textarea('description', $event->description,['class'=>'form-control', 'placeholder'=>'Write a description'])}}
                </div>
                {{Form::hidden('_method','PUT')}}
                {{ Form::submit('Submit', ['class'=>'btn btn-primary']) }}
