cdd2c229d36e5ce74898ef970a50c6d8bf5c0cee
juneju <djuneju@gmail.com>
2019-04-04 23:12:28 +0600

    Indentation corrected

--- EventBook/resources/views/pages/index.blade.php
+++ EventBook/resources/views/pages/index.blade.php
@@ -2,9 +2,9 @@
 @section('content')
 <div class ="jumbotron text-center">
 
-        <h1>Welcome to Eventbook</h1>
-<p>This is the largest platform for upcoming events.Book your events now</p>
-<p><a class ="btn btn-primary btn-lg" href="/login" role= "button">login</a> 
-    <a class= "btn btn-succes btn-lg" href= "/register" role= "button">Register</a>
-</p>
+    <h1>Welcome to Eventbook</h1>
+    <p>This is the largest platform for upcoming events.Book your events now</p>
+    <p><a class ="btn btn-primary btn-lg" href="/login" role= "button">login</a> 
+        <a class= "btn btn-succes btn-lg" href= "/register" role= "button">Register</a>
+    </p>
 @endsection
