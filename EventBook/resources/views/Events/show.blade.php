2b80088bc61d60b64342ab5bc9a050491e552ab6
juneju <djuneju@gmail.com>
2019-04-05 00:07:27 +0600

    Error Corrected

--- EventBook/resources/views/Events/show.blade.php
+++ EventBook/resources/views/Events/show.blade.php
@@ -10,7 +10,7 @@
         Venue: {{ $event->venue }} <br>
         Event Date: {{ $event->event_date }} <br>
         <b> Registration Deadline: {{ $event->reg_deadline}} </b><br> 
-        Event Type: {{$event->event_type}}
+        Event Type: {{$event->event_type}}<br>
         Description: {{ $event->description}}
         <hr>
         {!!Form::open(['action' => ['EventsController@destroy',$event->id],
