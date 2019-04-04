124b4e2f21460a0211976f05200ce49496133d76
juneju <djuneju@gmail.com>
2019-04-04 23:31:31 +0600

    Text Color Changed

--- EventBook/resources/views/pages/index.blade.php
+++ EventBook/resources/views/pages/index.blade.php
@@ -2,8 +2,8 @@
 @section('content')
 <div class ="text-center">
     <body background={{ asset('img/logo1.jpg')}}>
-        <h1>Welcome to Eventbook</h1>
-        <p>This is the largest platform for upcoming events.Book your events now</p>
+        <h1 style="color:lightblue;">Welcome to Eventbook</h1>
+        <p style="color:lightblue;">This is the largest platform for upcoming events.Book your events now</p>
         <p><a class ="btn btn-primary btn-lg" href="/login" role= "button">login</a> 
             <a class= "btn btn-succes btn-lg" href= "/register" role= "button">Register</a>
         </p>
