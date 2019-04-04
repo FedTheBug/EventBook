c1fecdd8dbdd1b4adb658e4fb4da8556cdfb5182
tapuzaman <tapu.zaman101@gmail.com>
2019-04-04 21:04:11 +0600

    Event Type added in Show

--- EventBook/resources/views/Events/show.blade.php
+++ EventBook/resources/views/Events/show.blade.php
@@ -10,6 +10,7 @@
         Venue: {{ $event->venue }} <br>
         Event Date: {{ $event->event_date }} <br>
         <b> Registration Deadline: {{ $event->reg_deadline}} </b><br><br> 
+        Event Type: {{$event->event_type}}
         Description: {{ $event->description}}
         <hr>
         {!!Form::open(['action' => ['EventsController@destroy',$event->id],
