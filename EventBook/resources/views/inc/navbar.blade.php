95eab5f6802370ae50f6137183f25ed45f4c6adb
juneju <djuneju@gmail.com>
2019-04-05 09:57:05 +0600

    Dashboard Auth Chnaged

--- EventBook/resources/views/inc/navbar.blade.php
+++ EventBook/resources/views/inc/navbar.blade.php
@@ -16,7 +16,7 @@
 
                 <ul class="navbar-nav">
             
-                  <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                  <a class="nav-link" href="/home">Home <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
                   <a class="nav-link" href="/about">About</a>
