4e193a1708581c7841ee59e383a1d82bbd329583
juneju <djuneju@gmail.com>
2019-04-05 13:18:32 +0600

    Backgorund Changed in Dashboard

--- EventBook/resources/views/home.blade.php
+++ EventBook/resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header" align="center">Dashboard</div>
+                <div class="card-header" style="background-color:lightblue" align="center">Dashboard</div>
 
                 <div class="card-body">
                     @if (session('status'))
