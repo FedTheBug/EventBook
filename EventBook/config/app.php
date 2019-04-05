53245a437ccdb0011fb0cfd87a3e379e73e38146
juneju <djuneju@gmail.com>
2019-04-06 01:23:52 +0600

    Aliases Added

--- EventBook/config/app.php
+++ EventBook/config/app.php
@@ -233,6 +233,8 @@ return [
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
 
+        'Socialite' => Laravel\Socialite\Facades\Socialite::class,
+
     ],
 
 ];
