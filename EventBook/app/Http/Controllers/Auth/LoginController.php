b7564bb431d8eaad64b7d8ff0d39bc260096a62a
juneju <djuneju@gmail.com>
2019-04-06 02:39:49 +0600

    redirectToProvider Method Defined

--- EventBook/app/Http/Controllers/Auth/LoginController.php
+++ EventBook/app/Http/Controllers/Auth/LoginController.php
@@ -39,4 +39,12 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    //
+    public function redirectToProvider($provider)
+    {
+        return Socialite::driver($provider)->redirect();
+    }
+
+
 }
