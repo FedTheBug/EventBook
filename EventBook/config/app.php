5c7930b1d1bea3c9aa27bdcd836960905280f089
tapuzaman <tapu.zaman101@gmail.com>
2019-04-07 00:03:50 +0600

    Registered Socialite to providers and aliases

--- EventBook/config/app.php
+++ EventBook/config/app.php
@@ -178,6 +178,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Laravel\Socialite\SocialiteServiceProvider::class,
 
     ],
 
@@ -231,6 +232,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Socialite' => Laravel\Socialite\Facades\Socialite::class,
 
     ],
 
