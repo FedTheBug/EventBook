5050f4d5d552ddeccdfccf8a188e4e70772bd3a5
tapuzaman <tapu.zaman101@gmail.com>
2019-04-07 00:19:52 +0600

    In Line Comment added

--- EventBook/config/app.php
+++ EventBook/config/app.php
@@ -178,6 +178,11 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+       
+        /*
+        *Socialite Service providers
+        */
+
         Laravel\Socialite\SocialiteServiceProvider::class,
 
     ],
@@ -232,6 +237,11 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+
+        /*
+        *Socialite Service Aliases
+        */
+
         'Socialite' => Laravel\Socialite\Facades\Socialite::class,
 
     ],
