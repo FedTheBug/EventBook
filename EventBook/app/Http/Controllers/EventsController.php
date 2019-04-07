9bacd9061fb220ecb4657de89d7f524c89ce6320
juneju <djuneju@gmail.com>
2019-04-08 01:12:25 +0600

    Event List Can View Without Login

--- EventBook/app/Http/Controllers/EventsController.php
+++ EventBook/app/Http/Controllers/EventsController.php
@@ -27,7 +27,7 @@ class EventsController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
     /**
