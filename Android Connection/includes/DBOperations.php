180bde3bd2286a70703408840f46da25912bf5ae
FedTheBug <fahadmuntasir747@gmail.com>
2019-04-12 10:44:56 +0600

    Middleware for CRUD Operations Added

---
+++ Android Connection/includes/DBOperations.php
@@ -0,0 +1,28 @@
+<?php
+/*
+*Middleware that connects to the DataBase
+*Does CRUD Operations
+*
+*/
+class DBOperations{
+    private $con;
+    function __construct(){
+        require_once dirname(__FILE__).'/DBConnect.php';
+        $db = new DBConnect();
+        $this->con = $db->connect();
+}
+    #creates new user
+    public function createUser($name, $password, $email){
+        if($this->isUserExist($name, $email)){
+            return 0;
+        }else{
+            $pass = password_hash($password, PASSWORD_DEFAULT);
+            $stmt = $this->con->prepare("INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES ('?', '?', '?', NULL, '?', NULL, NULL, NULL);");
+            $stmt->bind_param("sss", $name, $pass, $email);
+            if($stmt->execute()){
+                return 1;
+            }
+            else{
+                return 2;
+            }
+    }
