c8be7cf4be390e1f6e7465c8a6ba26955c931a14
juneju <djuneju@gmail.com>
2019-04-13 01:21:11 +0600

    Condition Changed RegisterUser

--- AndroidConnection/v1/registerUser.php
+++ AndroidConnection/v1/registerUser.php
@@ -20,26 +20,27 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
         #Operate the data further
         
         $db = new DBOperations();
-        $result = $db->createUser(
-                    $_POST['name'],
-                    $_POST['password'],
-                    $_POST['email']
-        );
-        if($result == 1){
+
+        if($db->createUser(
+                $_POST['name'],
+                $_POST['password'],
+                $_POST['email']
+        ))
+        {
             $response['error'] = false;
             $response['message'] = "User Created successfully!";
-        }else if($result == 2){
-            $response['error'] = true;
-            $response['message'] = "User not created";
-        }else if($result == 0){
-            $response['error'] = true;
-            $response['message'] = "Username or Email is already registered!";
         }
-    }else{
+        else{
+            $response['error'] = true;
+            $response['message'] = "Some error occured please try again";
+        }
+    }
+    else{
         $response['error'] = true;
         $response['message'] = "Required fields are missing";
     }
-}else{
+}
+else{
     $response['error'] = true;
     $response['message'] = "Invalid request";
 }
