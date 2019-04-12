f9e1c6b82cbdc20ae156bc92d399abc6e1f913f8
FedTheBug <fahadmuntasir747@gmail.com>
2019-04-12 09:52:25 +0600

    Constant Class Added

---
+++ Android Connection/includes/Constants.php
@@ -0,0 +1,12 @@
+
+<?php
+/*
+*
+*Contains necessary constants
+*
+*/
+
+	define('DB_NAME','eventbook');
+	define('DB_USER','root');
+	define('DB_PASSWORD','1234');
+	define('DB_host','localhost');
---
+++ Android Connection/includes/DBConnect.php
@@ -0,0 +1,8 @@
+
+<?php
+/*
+*
+*Connects to the DataBase
+*Returns link for the DB
+*
+*/
