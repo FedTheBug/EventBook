0287b96f663bd0844c97aaf133800dd54231b1a7
juneju <djuneju@gmail.com>
2019-04-13 20:31:18 +0600

    Constants Class Added

---
+++ AndroidApp/app/src/main/java/com/example/eventbook/Constants.java
@@ -0,0 +1,11 @@
+package com.example.eventbook;
+
+public class Constants {
+
+    private static final String ROOT_URL = "http://192.168.0.5/AndroidConnection/v1/";
+
+    public static final String URL_REGISTER = ROOT_URL+"registerUser.php";
+
+    //public static final String URL_LOGIN = ROOT_URL+"userLogin.php";
+}
+
