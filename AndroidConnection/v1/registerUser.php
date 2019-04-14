f08b0e40b5bc799d4b64198320f9ab471abebc77
juneju <djuneju@gmail.com>
2019-04-14 10:29:26 +0600

    Code Mofidied in RegiterUser

--- AndroidConnection/v1/registerUser.php
+++ AndroidConnection/v1/registerUser.php
@@ -1,47 +1,41 @@
-
-<?php
-
-/*
-*
-* Gives HTTP POST Request
-* Returns a Response
-*
-*/
-require_once '../includes/DBOperations.php';
-$response = array();
-
-
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    if(isset($_POST['name']) and
-         isset($_POST['email']) and
-             isset($_POST['password'])
-    )
-    {
-        #Operate the data further
-        
-        $db = new DBOperations();
-
-        if($db->createUser(
-                $_POST['name'],
-                $_POST['password'],
-                $_POST['email']
-        ))
+<?php 
+ 
+require_once '../includes/DbOperations.php';
+ 
+$response = array(); 
+ 
+if($_SERVER['REQUEST_METHOD']=='POST'){
+    if(
+        isset($_POST['name']) and
+            isset($_POST['email']) and
+                isset($_POST['password']))
         {
-            $response['error'] = false;
-            $response['message'] = "User Created successfully!";
+        //operate the data further 
+ 
+        $db = new DbOperations(); 
+ 
+        $result = $db->createUser(   $_POST['name'],
+                                    $_POST['password'],
+                                    $_POST['email']
+                                );
+        if($result == 1){
+            $response['error'] = false; 
+            $response['message'] = "User registered successfully";
+        }elseif($result == 2){
+            $response['error'] = true; 
+            $response['message'] = "Some error occurred please try again";          
+        }elseif($result == 0){
+            $response['error'] = true; 
+            $response['message'] = "It seems you are already registered, please choose a different email and username";                     
         }
-        else{
-            $response['error'] = true;
-            $response['message'] = "Some error occured please try again";
-        }
-    }
-    else{
-        $response['error'] = true;
+ 
+    }else{
+        $response['error'] = true; 
         $response['message'] = "Required fields are missing";
     }
+}else{
+    $response['error'] = true; 
+    $response['message'] = "Invalid Request";
 }
-else{
-    $response['error'] = true;
-    $response['message'] = "Invalid request";
-}
+ 
 echo json_encode($response);
