4b1744f6af238d4a76d8a511c893cf7849b16400
juneju <djuneju@gmail.com>
2019-04-14 13:18:29 +0600

    Login Feature Implemented in Android App

--- AndroidApp/app/src/main/java/com/example/eventbook/MainActivity.java
+++ AndroidApp/app/src/main/java/com/example/eventbook/MainActivity.java
@@ -6,8 +6,6 @@ import android.os.Bundle;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,11 +14,9 @@ import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
-import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -114,7 +110,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         if (view == buttonRegister)
             registerUser();
-        if(view == textViewLogin)
-            startActivity(new Intent(this, Login.class));
+       if(view == textViewLogin)
+            startActivity(new Intent(this, LoginActivity.class));
     }
 }
