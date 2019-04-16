949a538f5b5c792ca3587ec1991e78f9e9a31a76
FedTheBug <fahadmuntasir747@gmail.com>
2019-04-16 23:49:49 +0600

    Div Class for Showing Image

--- EventBook/resources/views/Events/index.blade.php
+++ EventBook/resources/views/Events/index.blade.php
@@ -7,6 +7,14 @@
     @if(count($events)>0)
     @foreach($events as $event)
         <div class= 'well' >
+            <div class="row">
+                <div class="col-md-4 col-sm-4">
+
+                </div>
+                <div class="col-md-8 col-sm-8">
+
+                </div>
+            </div>
             <h3><a href ="/events/{{$event->id}}">{{$event->name}}</a></h3>
             <small>Event Date: {{$event->event_date}}</small>
             <hr>
