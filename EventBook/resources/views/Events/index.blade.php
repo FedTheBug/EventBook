9979e1c61fd6e0a53f7dfe0384d5bf8768ebd6e1
FedTheBug <fahadmuntasir747@gmail.com>
2019-04-16 23:53:58 +0600

    Image Added to Events

--- EventBook/resources/views/Events/index.blade.php
+++ EventBook/resources/views/Events/index.blade.php
@@ -9,7 +9,7 @@
         <div class= 'well' >
             <div class="row">
                 <div class="col-md-4 col-sm-4">
-
+                    <img style="width:100%" src="/storage/cover_images/{{$event->cover_image}}">
                 </div>
                 <div class="col-md-8 col-sm-8">
                         <h3><a href ="/events/{{$event->id}}">{{$event->name}}</a></h3>
