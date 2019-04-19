ffd9de3964e64347127c74c158a040c5e1bb3d52
juneju <djuneju@gmail.com>
2019-04-19 13:13:42 +0600

    Message Modified

--- AndroidConnection/includes/DBConnect.php
+++ AndroidConnection/includes/DBConnect.php
@@ -15,7 +15,7 @@ class DBConnect{
         include_once dirname(__FILE__).'/Constants.php';
         $this->con = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
         if(mysqli_connect_errno()){
-            echo "Failed to connect with database".mysqli_connect_err();
+            echo "Failed to connect with database.".mysqli_connect_err();
         }
         return $this->con;
     }
