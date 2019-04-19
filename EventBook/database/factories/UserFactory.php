7832a4004814156516473bff788ed6ab36cf456b
FedTheBug <fahadmuntasir747@gmail.com>
2019-04-20 00:00:28 +0600

    Unit Test for Events

--- EventBook/database/factories/UserFactory.php
+++ EventBook/database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 use App\User;
 use App\Event;
+use Carbon\Carbon;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
 
@@ -28,8 +29,11 @@ $factory->define(User::class, function (Faker $faker) {
 });
 $factory->define(User::class, function (Faker $faker) {
     return [
+        'id' => 1,
         'name' => $faker->name,
         'venue' => $faker->name,
+        'event_date' => Carbon::today(),
+        'reg_deadline' => Carbon::tomorrow(),
         'description' => $fake->paragraph,
     ];
 
