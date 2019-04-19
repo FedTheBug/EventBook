04e4497f5de0116e7c2330180c7aaf35e32ab045
juneju <djuneju@gmail.com>
2019-04-19 13:33:23 +0600

    Few Code Modified

--- AndroidApp/app/src/main/java/com/example/eventbook/Constants.java
+++ AndroidApp/app/src/main/java/com/example/eventbook/Constants.java
@@ -2,11 +2,11 @@ package com.example.eventbook;
 
 public class Constants {
 
-    private static final String ROOT_URL = "http://192.168.0.5/AndroidConnection/v1/";
+    private static final String ROOT_URL = "http://192.168.42.122/AndroidConnection/v1/";
 
+    public static final String URL_ALL_EVENTS = ROOT_URL+"allEvents.php";
     public static final String URL_REGISTER = ROOT_URL+"registerUser.php";
     public static final String URL_LOGIN = ROOT_URL+"userLogin.php";
-    public static final String URL_ALL_EVENTS = ROOT_URL+"allEvents.php";
 
 }
 
