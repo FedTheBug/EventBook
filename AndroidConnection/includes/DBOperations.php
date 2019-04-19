60d063f35edb51894e416a84d5b2beb2767735d8
juneju <djuneju@gmail.com>
2019-04-19 11:10:04 +0600

    Class Name Changed

--- AndroidConnection/includes/DBOperations.php
+++ AndroidConnection/includes/DBOperations.php
@@ -1,12 +1,12 @@
 <?php 
  
-    class DbOperations{
+    class DBOperations{
  
         private $con; 
  
         function __construct(){
  
-            require_once dirname(__FILE__).'/DbConnect.php';
+            require_once dirname(__FILE__).'/DBConnect.php';
             $db = new DbConnect();
             $this->con = $db->connect();
         }
