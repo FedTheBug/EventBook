abe3222983607397a2a1855c3778522f98eb8631
juneju <djuneju@gmail.com>
2019-04-19 17:39:37 +0600

    Mobile Vision Removed

--- AndroidApp/app/src/main/java/com/example/eventbook/Constants.java
+++ AndroidApp/app/src/main/java/com/example/eventbook/Constants.java
@@ -2,7 +2,7 @@ package com.example.eventbook;
 
 public class Constants {
 
-    private static final String ROOT_URL = "http://192.168.42.122/AndroidConnection/v1/";
+    private static final String ROOT_URL = "http://192.168.0.5/AndroidConnection/v1/";
 
     public static final String URL_ALL_EVENTS = ROOT_URL+"allEvents.php";
     public static final String URL_REGISTER = ROOT_URL+"registerUser.php";
