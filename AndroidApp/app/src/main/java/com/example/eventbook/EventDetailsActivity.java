eccbe97c6b441f9e5ce03eb548e9c8bf3d3b843f
juneju <djuneju@gmail.com>
2019-04-19 22:25:59 +0600

    Variable Defined For Text View

--- AndroidApp/app/src/main/java/com/example/eventbook/EventDetailsActivity.java
+++ AndroidApp/app/src/main/java/com/example/eventbook/EventDetailsActivity.java
@@ -2,12 +2,24 @@ package com.example.eventbook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class EventDetailsActivity extends AppCompatActivity {
+
+    private TextView textViewEventName, textViewVenue,
+            textViewEventDate, textViewRegDeadline, textViewDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_details);
+
+        textViewEventName = (TextView) findViewById(R.id.textViewEventName);
+        textViewVenue = (TextView) findViewById(R.id.textViewVenue);
+        textViewEventDate = (TextView) findViewById(R.id.textViewEventDate);
+        textViewRegDeadline = (TextView) findViewById(R.id.textViewRegDeadline);
+        textViewDescription = (TextView) findViewById(R.id.textViewDescription);
+
     }
-}
+
+
