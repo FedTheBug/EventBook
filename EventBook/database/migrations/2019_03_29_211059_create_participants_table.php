b89d22827e8a2e7f1a7edb796ed458c2011671d7
FedTheBug <fahadmuntasir747@gmail.com>
2019-04-20 20:59:46 +0600

    Source Code Comment Added

--- EventBook/database/migrations/2019_03_29_211059_create_participants_table.php
+++ EventBook/database/migrations/2019_03_29_211059_create_participants_table.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * 
+ * Migration Class
+ * Creates participants class in the Database
+ * 
+ */
+
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
