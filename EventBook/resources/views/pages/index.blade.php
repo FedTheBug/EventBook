7db5313e6e2610dc32059ceb19b3b3ba73db41ff
juneju <djuneju@gmail.com>
2019-04-25 00:43:17 +0600

    New Web UI Implemented

--- EventBook/resources/views/pages/index.blade.php
+++ EventBook/resources/views/pages/index.blade.php
@@ -1,16 +1,62 @@
 @extends('layouts.app')
 @section('content')
-<div class ="text-center">
-    <body background={{ asset('img/logo1.jpg')}}>
-        <h1 style="color:lightblue;">Welcome to Eventbook!</h1>
-        <p style="color:lightblue;">This is the largest platform for upcoming events. Book your events now.</p>
+
+    <head>
+            <link href="{{ asset('css/min.css') }}" rel="stylesheet">
+            <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+            <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+            <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    </head>
+
+    <body background={{ asset('img/black.jpg')}}>
+
+    <div style = "position:relative;height:900px ">
+    <svg viewBox="0 0 1800 600">
+            <symbol id="s-text">
+                <text text-anchor="middle"
+                    x="50%"
+                    y="35%"
+                    class="webcoderskull"
+                    >
+                Welcome to 
+                </text>
+                <text text-anchor="middle"
+                    x="50%"
+                    y="68%"
+                    class="text--line"
+                    >
+                Eventbook!
+                </text>
+                
+            </symbol>
+            
+            <g class="g-ants">
+                <use xlink:href="#s-text"
+                class="webcoderskull-1"></use>     
+                <use xlink:href="#s-text"
+                class="webcoderskull-1"></use>     
+                <use xlink:href="#s-text"
+                class="webcoderskull-1"></use>     
+                <use xlink:href="#s-text"
+                class="webcoderskull-1"></use>     
+                <use xlink:href="#s-text"
+                class="webcoderskull-1"></use>     
+            </g>
+    </svg>
+    
+    <div style = "position:relative; left:40%; top:800px;">
+           
         <p><a class ="btn btn-primary btn-lg" href="/login" role= "button">Login</a> 
             <a class= "btn btn-warning btn-lg" href= "/register" role= "button">Register</a>
         </p>
         <p>
             <a class= "btn btn-danger btn-lg" href= "/auth/google" role= "button">Login with Google</a>
         </p>
+    </div>
+        
+        
+    </div>
     </body>
-</div>
+
 @endsection
 
