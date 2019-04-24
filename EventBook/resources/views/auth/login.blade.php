eb2703ea49650b4e2f3986fc966af23d3abb41f1
juneju <djuneju@gmail.com>
2019-04-25 02:19:38 +0600

    App Login UI Changed

--- EventBook/resources/views/auth/login.blade.php
+++ EventBook/resources/views/auth/login.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+
+<body background={{ asset('img/blur.jpg')}}>
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -72,4 +75,6 @@
         </div>
     </div>
 </div>
+
+</body>
 @endsection
