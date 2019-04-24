8615217b821293a489825354cd25bd7ee724a671
juneju <djuneju@gmail.com>
2019-04-25 02:17:40 +0600

    UI Changed For Registration Page

--- EventBook/resources/views/auth/register.blade.php
+++ EventBook/resources/views/auth/register.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+
+<body background={{ asset('img/blur.jpg')}}>
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -76,4 +79,6 @@
         </div>
     </div>
 </div>
+
+</body>
 @endsection
