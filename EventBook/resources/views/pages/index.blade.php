458b089050a139b5b154dd328c660565944ed483
juneju <djuneju@gmail.com>
2019-04-25 02:50:56 +0600

    Index UI Updated

--- EventBook/resources/views/pages/index.blade.php
+++ EventBook/resources/views/pages/index.blade.php
@@ -6,11 +6,32 @@
             <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
             <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
             <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+            <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+            <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+            <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     </head>
 
     <body background={{ asset('img/black.jpg')}}>
 
-    <div style = "position:relative;height:900px ">
+    <div style = "position:relative;top:120px">
+    <div class="container">
+        <div class="row">
+            <div class="container">
+                <div class="row">
+                    <a href="#" class="intro-banner-vdo-play-btn pinkBg" target="_blank">
+                    <i class="glyphicon glyphicon-play whiteText" aria-hidden="true"></i>
+                    <span class="ripple pinkBg"></span>
+                    <span class="ripple pinkBg"></span>
+                    <span class="ripple pinkBg"></span>
+                    </a>
+                </div>
+            </div>
+        </div>    
+    </div>
+</div>
+    
+    <div style = "position:relative;height:900px">
     <svg viewBox="0 0 1800 600">
             <symbol id="s-text">
                 <text text-anchor="middle"
